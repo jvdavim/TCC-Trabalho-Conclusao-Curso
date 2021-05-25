cb7cc0547c31d8026e82fedf2c0a78603e08f4ea
Joao Victor Davim <joaovictor_rj@poli.ufrj.br>
2021-05-25 00:48:29 -0300

    replace minimum-daily-temperatures with only temperatures

--- src/utils/data_temperatures.py
+++ src/utils/data_temperatures.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     df = pd.read_csv("https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv")
     series = pd.Series(data=df["Temp"].values, index=df["Date"].values)
     plot_time_series(
-        "data/bench/img/minimum_daily_temperatures.png",
+        "data/bench/img/temperatures.png",
         series.index,
         series.values,
         "Minimum Daily Temperatures")
-    series.to_csv("data/bench/minimum_daily_temperatures.csv")
+    series.to_csv("data/bench/temperatures.csv")
